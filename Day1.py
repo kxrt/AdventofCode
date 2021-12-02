7217d7e0306d7678c11ca5ef629cd510e1def458
kxrt <73589885+kxrt@users.noreply.github.com>
2021-12-02 13:59:37 +0800

    first commit

---
+++ Day1.py
@@ -0,0 +1,23 @@
+# Part 1
+
+import numpy as np
+
+arr = np.genfromtxt('Day1_input.txt')
+
+def increasing(array): 
+    result = 0
+    for i in range(1, len(array)):
+        if array[i] > array[i - 1]:
+            result += 1
+    return result 
+
+print(increasing(arr)) # Ans: 1713
+
+# Part 2
+
+newarr = []
+
+for x in range(0, len(arr) - 2):
+    newarr.append(arr[x] + arr[x + 1] + arr[x + 2])
+
+print(increasing(newarr)) # Ans: 1734
